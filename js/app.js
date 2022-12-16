45778cca202f0039967a62059d00866e6ddd0f5c
Megan Smith <megans766@gmail.com>
2022-12-16 15:31:46 -0500

    set up initial game state

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*------------------------------------------Constants------------------------------------------*/
+const winOutcomes = []
+
+/*------------------------------------------Variables------------------------------------------*/
+let gameBoard, playerTurn, player1, theWinner
+
+/*------------------------------------------Cached---------------------------------------------*/
+
+const tileEls = document.querySelectorAll('.tile')
+const displayMessage = document.getElementById('game-result')
+
+/*------------------------------------------Event Listeners------------------------------------*/
+
+
+/*------------------------------------------Functions------------------------------------------*/
+
+function start() {
+    gameBoard = 0
+    playerTurn = 1
+    theWinner - false
+}
+
+function rendor() {
+
+}
