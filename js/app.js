36d7902cca19d4a9a223a0384a439fe1af5f712f
Megan Smith <megans766@gmail.com>
2022-12-19 10:10:31 -0500

    fix winning outcomes array

--- js/app.js
+++ js/app.js
@@ -1,31 +1,43 @@
 /*------------------------------------------Constants------------------------------------------*/
+const boardColumns = [
+    [tileEls[0], tileEls[7], tileEls[14], tileEls[21], tileEls[28], tileEls[35]],
+    [tileEls[1], tileEls[8], tileEls[15], tileEls[22], tileEls[29], tileEls[36]],
+    [tileEls[2], tileEls[9], tileEls[16], tileEls[23], tileEls[30], tileEls[37]],
+    [tileEls[3], tileEls[10], tileEls[17], tileEls[24], tileEls[31], tileEls[38]],
+    [tileEls[4], tileEls[11], tileEls[18], tileEls[25], tileEls[32], tileEls[39]],
+    [tileEls[5], tileEls[12], tileEls[19], tileEls[26], tileEls[33], tileEls[40]],
+    [tileEls[6], tileEls[13], tileEls[20], tileEls[27], tileEls[34], tileEls[41]],
+]
+
+
 const winOutcomes = [
     //winning combos down
-    [1, 8, 15, 22], [8, 15, 22, 29], [15, 22, 29, 36], [2, 9, 16, 23], [9, 16, 23, 30], [16, 23, 30, 37], [3, 10, 17, 24],
-    [10, 17, 24, 31], [17, 24, 31, 38], [4, 11, 18, 25], [11, 18, 25, 32], [18, 25, 32, 39], [5, 12, 19, 26], [12, 19, 26, 33],
-    [19, 26, 33, 40], [6, 13, 20, 27], [13, 20, 27, 34], [20, 27, 34, 41], [7, 14, 21, 28], [14, 21, 28, 35], [21, 28, 35, 42],
+    [0, 7, 14, 21], [7, 14, 21, 28], [14, 21, 28, 35], [1, 8, 15, 22], [8, 15, 22, 29], [15, 22, 29, 36], [2, 9, 16, 23],
+    [9, 16, 23, 30], [16, 23, 30, 37], [3, 10, 17, 24], [10, 17, 24, 31], [17, 24, 31, 38], [4, 11, 18, 25], [11, 18, 25, 32],
+    [18, 25, 32, 39], [5, 12, 19, 26], [12, 19, 26, 33], [19, 26, 33, 40], [6, 13, 20, 27], [13, 20, 27, 34], [20, 27, 34, 41],
     //winning combos across
-    [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [8, 9, 10, 11], [9, 10, 11, 12], [10, 11, 12, 13], 
-    [11, 12, 13, 14], [15, 16, 17, 18], [16, 17, 18, 19], [17, 18, 19, 20], [18, 19, 20, 21], [22, 23, 24, 25], 
-    [23, 24, 25, 26], [24, 25, 26, 27], [25, 26, 27, 28], [29, 30, 31, 32], [30, 31, 32, 33], [31, 32, 33, 34], 
-    [32, 33, 34, 35], [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41], [39, 40, 41, 42],
+    [0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [7, 8, 9, 10], [8, 9, 10, 11], [9, 10, 11, 12], 
+    [10, 11, 12, 13], [14, 15, 16, 17], [15, 16, 17, 18], [16, 17, 18, 19], [17, 18, 19, 20], [21, 22, 23, 24], 
+    [22, 23, 24, 25], [23, 24, 25, 26], [24, 25, 26, 27], [28, 29, 30, 31], [29, 30, 31, 32], [30, 31, 32, 33], 
+    [31, 32, 33, 34], [35, 36, 37, 38], [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41],
     //winning combos across
-    [15, 23, 31, 39], [21, 27, 33, 39], [8, 16, 24, 32], [16, 24, 32, 40], [14, 20, 26, 32], [20, 26, 32, 38], [1, 9, 17, 25],
-    [9, 17, 25, 33], [17, 25, 33, 41], [7, 13, 19, 25], [13, 19, 25, 31], [19, 25, 31, 37], [2, 10, 18, 26], [10, 18, 26, 34],
-    [18, 26, 34, 42], [3, 11, 19, 27], [11, 19, 27, 35], [6, 12, 18, 24], [12, 18, 24, 30], [18, 24, 30, 36], [5, 11, 17, 23],
-    [11, 17, 23, 29], [4, 10, 16, 22], [4, 12, 20, 28]
+    [14, 22, 30, 38], [20, 26, 32, 38], [7, 15, 23, 31], [15, 23, 31, 39], [13, 19, 25, 31], [19, 25, 31, 37], [0, 8, 16, 24],
+    [8, 16, 24, 32], [16, 24, 32, 40], [6, 12, 18, 24], [12, 18, 24, 30], [18, 24, 30, 36], [1, 9, 17, 25], [9, 17, 25, 33],
+    [17, 25, 33, 41], [2, 10, 18, 26], [10, 18, 26, 34], [5, 11, 17, 23], [11, 17, 23, 29], [17, 23, 29, 35], [4, 10, 16, 22],
+    [10, 16, 22, 28], [3, 9, 15, 21], [3, 11, 19, 27]
 ]
 
 /*------------------------------------------Variables------------------------------------------*/
 let gameBoard, playerTurn, tie, theWinner
 
 /*------------------------------------------Cached---------------------------------------------*/
-const gameBoardEls = document.querySelector('.game-board')
 const tileEls = document.querySelectorAll('.tile')
 const displayMessage = document.getElementById('game-result')
+const columnEls = document.querySelectorAll('.column')
 
 /*------------------------------------------Event Listeners------------------------------------*/
-gameBoardEls.addEventListener('click', handleClick)
+// tileEls.addEventListener('click', handleClick)
+// columnEls.addEventListener('click', '')
 
 /*------------------------------------------Functions------------------------------------------*/
 start()
@@ -43,76 +55,90 @@ function start() {
     tie= false
     theWinner = false
     render()
-    gameStartMessage()
+    // gameStartMessage()
 }
 
 function render() {
-    gameBoardPlay()
-    outcomeMessage()
+    // gameBoardPlay()
+    // outcomeMessage()
 }
 
-function gameBoardPlay() {
-    gameBoard.forEach(function(slot, idx) {
-        if (slot === 1) {
-            return tileEls[idx].innerHTML = "player1"
-        }else if (slot === -1) {
-            return tileEls[idx].innerHTML = "player2"
-        }else {
-            return tileEls[idx].innerHTML = ""
-        }
-    })
+// function gameBoardPlay() {
+//     gameBoard.forEach(function(slot, idx) {
+//         if (slot === 1) {
+//             return tileEls[idx].innerHTML = "player1"
+//         }else if (slot === -1) {
+//             return tileEls[idx].innerHTML = "player2"
+//         }else {
+//             return tileEls[idx].innerHTML = ""
+//         }
+//     })
+// }
+
+// function handleClick(evt) {
+//     const boardIdx = evt.target.id
+
+//     if (theWinner === true) {
+//         return
+//     }
+//     if (gameBoard[boardIdx]) {
+//         return
+//     }
+//     gameToken(boardIdx)
+//     tieGame()
+//     changeTurn()
+//     render()
+//     console.log(boardIdx);
+// }
+
+// function gameStartMessage() {
+//     gameBoard.forEach(function(slot) {
+//         if (slot === null) {
+//             displayMessage.textContent = "Click a token below to begin"
+//         }
+//     })
+// }
+
+// function outcomeMessage() {
+//     if (theWinner === false && tie === false) {
+//         displayMessage.textContent = `Player ${playerTurn === 1? 'Player1' : 'Player2'} turn`
+//     }else if (theWinner === false && tie === true) {
+//         displayMessage.textContent = "It's a tie!"
+//     }else {
+//         displayMessage.textContent = `Player ${playerTurn === -1? 'Player1' : 'Player2'} wins!`
+//     }
+// }
+
+// function changeTurn() {
+//     if (theWinner === true) {
+//         return 
+//     }else {
+//         playerTurn = playerTurn * -1
+//     }
+// }
+
+// function gameToken(idx) {
+//     gameBoard[idx] = playerTurn
+//     console.log(gameBoard);
+// }
+
+// function tieGame() {
+//     tie = gameBoard.every(function(slot) {
+//         return slot !== null 
+//     })
+// }
+
+// function gameWinner() {
+    
+// }
+
+// function dropToken() {
+
+// }
+
+function dropToken() {
+    let tokenPlacement = boardColumns
+
+    if ()
+
 }
-
-function handleClick(evt) {
-    const boardIdx = evt.target.id
-
-    if (theWinner === true) {
-        return
-    }
-    if (gameBoard[boardIdx]) {
-        return
-    }
-    gameToken(boardIdx)
-    tieGame()
-    changeTurn()
-    render()
-    console.log(boardIdx);
-}
-
-function gameStartMessage() {
-    gameBoard.forEach(function(slot) {
-        if (slot === null) {
-            displayMessage.textContent = "Click a tiki to begin"
-        }
-    })
-}
-
-function outcomeMessage() {
-    if (theWinner === false && tie === false) {
-        displayMessage.textContent = `Player ${playerTurn === 1? 'Player1' : 'Player2'} turn`
-    }else if (theWinner === false && tie === true) {
-        displayMessage.textContent = "It's a tie!"
-    }else {
-        displayMessage.textContent = `Player ${playerTurn === -1? 'Player1' : 'Player2'} wins!`
-    }
-}
-
-function changeTurn() {
-    if (theWinner === true) {
-        return 
-    }else {
-        playerTurn = playerTurn * -1
-    }
-}
-
-function gameToken(idx) {
-    gameBoard[idx] = playerTurn
-    console.log(gameBoard);
-}
-
-function tieGame() {
-    tie = gameBoard.every(function(slot) {
-        return slot !== null 
-    })
-}
-
